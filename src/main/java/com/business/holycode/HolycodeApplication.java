cc251b2812670164e0ac8b9d8a957f85e36e5e4e
mstepanovic <marko.stepanovic@icentic.com>
2022-12-03 15:16:35 +0100

    Initial commit

---
+++ src/main/java/com/business/holycode/HolycodeApplication.java
@@ -0,0 +1,13 @@
+package com.business.holycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolycodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolycodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/business/holycode/HolycodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business.holycode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolycodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
